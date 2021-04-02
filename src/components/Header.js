1c4937404e7e029e5dce41c907ed22c434fa1f1b
Ranjitha-06 <v.ranjitha06@gmail.com>
2021-04-02 14:25:30 +0530

    added responsive navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,26 @@ import {Link} from 'react-router-dom';
 export const Header =() => {
     return (
         
-     <nav>
-    <div className="nav-wrapper #e57373">
-      <a href="/" className="brand-logo  move "><i class="material-icons">all_inclusive</i>Infinity-Images</a>
-      <ul id="nav-mobile " class="right hide-on-med-and-down moveme">
+    /* <nav>
+    <div className="nav-wrapper red">
+      <a href="/" className="brand-logo hide-on-med-and-down move center"><i class="material-icons">all_inclusive</i></a>
+      <div className="center"></div>
+      <ul id="nav-mobile" class="center moveme">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/About">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
-    </div>
-  </nav>
-  
+      </div>
+  </nav>*/
+  <div className="header">
+  <a href="/" className="logo"><i className="material-icons moveme">all_inclusive</i>Infinity</a>
+      
+  <div className="header-right">
+    <a className="" href="/">Home</a>
+    <a href="/About">About</a>
+    <a href="/contact">Contact</a>
+  </div>
+  </div>
  
         
     )
